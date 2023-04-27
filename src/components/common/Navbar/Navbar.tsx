24285db9f5fbab17e9069947a4aa22523c47c74f
Ehrlacker <101287793+Ehrlacker@users.noreply.github.com>
2023-04-27 10:50:46 -0400

    add hamburger menu

--- src/components/common/Navbar/Navbar.tsx
+++ src/components/common/Navbar/Navbar.tsx
@@ -1,12 +1,23 @@
-import React from "react"
+import { useState } from "react"
+import Hamburger from "hamburger-react"
 import NavItem from "components/common/Navitem/Navitem"
 import "./Navbar.css"
 
 const Navbar = () => {
+  const [isOpen, setOpen] = useState(true)
+
+  if (isOpen) {
+    return (
+      <div className="NavbarHidden">
+        <Hamburger color="#C84B31" toggled={isOpen} toggle={setOpen} />
+        <NavItem page="/Parks" title="Parks" />
+        <NavItem page="/plans" title="Plans" />
+      </div>
+    )
+  }
   return (
     <div className="Navbar">
-      <NavItem page="/Parks" title="Parks" />
-      <NavItem page="/plans" title="Plans" />
+      <Hamburger color="#C84B31" toggled={isOpen} toggle={setOpen} />
     </div>
   )
 }
